c438232eb5dee9918a3910155fd416209b590530
Damian-Skotnicki <112865579+Damian-Skotnicki@users.noreply.github.com>
2022-09-30 11:29:56 +0100

    Add files via upload

---
+++ ASSIGNMENTS/learning maths.js
@@ -0,0 +1,114 @@
+//single line text (invisable)
+console.log("hello") 
+/* multi 
+line text (invisable)
+*/
+
+//and = &&
+
+//Variables
+var X= 3;
+let Y= 4;
+
+//Subtraction
+console.log(4-4);
+
+//addition
+console.log(4+4);
+
+//multiplication
+console.log(4*4);
+
+//to the power
+console.log(4**4);
+
+//division
+console.log(8/4);
+
+//equals
+console.log(4==4);
+//true
+
+//Not equals
+console.log(4!==4);
+//false
+
+//greater than
+console.log(5>6);
+
+//less than
+console.log(5<6);
+
+//greater than or equal
+console.log(6 >=6);
+
+//less than or equal
+console.log(2<= 1);
+
+
+/* assighments */
+
+//Add and assignment
+let A= 12;
+A +=12; 
+console.log(A);
+
+//subtract and assignment
+let B= 12;
+B -=12; 
+console.log(B);
+
+//multiply and assignment
+let C= 12;
+C *=12; 
+console.log(C);
+
+//divide and assignment
+let D= 12;
+D /=12; 
+console.log(D);
+
+//modulus and assignment
+let E= 12;
+E %= 12;
+console.log(E);
+
+
+//Condition opertator?
+let First= 10;
+let Second= 12;
+console.log(First>Second?"True":"False");
+
+// typeof operator (tells if number or string)
+let myString = "hello";
+console.log(typeof(myString));
+
+let myNumber = 12;
+console.log(typeof(myNumber));
+
+//user imput @
+/*
+let MySecondSting =prompt("Enter something");
+console.log(MySecondSting);
+
+let MySecondNumber =Number(prompt("enter a number"));
+console.log(MySecondNumber);
+*/
+
+//if statement
+let myage = 400
+//true
+if(myage > 500){ 
+    console.log("you are over 500 years old")  }
+    else{ console.log("you are NOT over 500 years old") }
+//false
+if(myage < 500){ 
+    console.log("you are over 500 years old")  }
+    else{ console.log("you are NOT over 500 years old") }
+
+ // if else-if else statement
+ if(myage > 500){ 
+     console.log("you are over 500 years old") 
+     } else if(myage<500){ console.log("you are less over 500 years old") 
+     } else{ console.log("you are Must over 500 years old") }
+ 
